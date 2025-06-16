d9b4b1860859e7f89ead5bc55939e37be1cb46dc
Metehan Gezer <irrelevantdev@outlook.com>
2025-06-16 17:44:39 +0300

    Install openssl 3 instead

--- scripts/macos/install-glist.sh
+++ scripts/macos/install-glist.sh
@@ -13,7 +13,7 @@ if [[ "$?" -ne 0 ]] ; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 # Install the required packages
-brew install git openssl@1.1 libomp llvm cmake glew glfw glm freetype assimp curl git wget pkg-config
+brew install git openssl@3 libomp llvm cmake glew glfw glm freetype assimp curl git wget pkg-config
 sudo spctl --master-disable
 
 # todo check if command line tools are properly installed and delete if required!
@@ -77,7 +77,7 @@ if [[ $(uname -p) == 'arm' ]]; then
     echo "Creating a shortcut to Eclipse in Applications folder"
     sudo ln -s ~/dev/glist/zbin/glistzbin-macos/eclipse/$ECLIPSE_FOLDER/Eclipse.app "/Applications/GlistEngine Eclipse"
     echo "OPENSSL VER:"
-    ls /opt/homebrew/Cellar/openssl@1.1
+    ls /opt/homebrew/Cellar/openssl@3
     echo "LLVM VER:"
     ls /opt/homebrew/Cellar/llvm
     # add to path
@@ -107,7 +107,7 @@ else
     echo "Creating a shortcut to Eclipse in Applications folder"
     sudo ln -s ~/dev/glist/zbin/glistzbin-macos-x86_64/eclipse/$ECLIPSE_FOLDER/Eclipse.app "/Applications/GlistEngine Eclipse"
     echo "OPENSSL VER:"
-    ls /usr/local/Cellar/openssl@1.1
+    ls /usr/local/Cellar/openssl@3
     echo "LLVM VER:"
     ls /usr/local/Cellar/llvm
     # add to path
